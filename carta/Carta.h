d69c058b5debec2572396e3a5d68af0eb2adc888
stefanofa <rasher@hotmail.it>
2018-08-24 18:34:21 +0200

    next

---
+++ carta/Carta.h
@@ -0,0 +1,14 @@
+//
+// Created by stefanofa on 24/08/18.
+//
+
+#ifndef GAMEOFTHEMADGOOSE_CARTA_H
+#define GAMEOFTHEMADGOOSE_CARTA_H
+
+
+class Carta {
+
+};
+
+
+#endif //GAMEOFTHEMADGOOSE_CARTA_H
